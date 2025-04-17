b12016c880802556dbd170f2bff4650bb33ab9b1
Evelyn Avramuti <evelynav@edu.aau.at>
2025-04-17 13:25:01 +0200

    #19-selection_of_unique_player_color: - Added five lighter player colors - Changed Type NONE to color GREY

--- src/main/java/com/example/cataniaunited/player/PlayerColor.java
+++ src/main/java/com/example/cataniaunited/player/PlayerColor.java
@@ -1,13 +1,18 @@
 package com.example.cataniaunited.player;
 
 public enum PlayerColor {
-    NONE("#CCCCCC"),
     RED("#BD2828"),
-    BLUE("#0080B3"),
-    YELLOW("#DB9224"),
+    LIGHT_RED("FF9999"),
     PURPLE("#B62FB2"),
+    LAVENDER("CCCCFF"),
+    BLUE("#0080B3"),
+    LIGHT_BLUE("B1DAE7"),
     GREEN("#008000"),
-    ORANGE("#CF6D17");
+    LIGHT_GREEN("A7F1A7"),
+    ORANGE("#CF6D17"),
+    LIGHT_ORANGE("FFCC99"),
+    YELLOW("#DB9224"),
+    GREY("#999999");
 
     private final String hexCode;
     PlayerColor(String hexCode) {
