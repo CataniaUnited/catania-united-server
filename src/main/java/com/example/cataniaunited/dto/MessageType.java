92b7f2646eb7b457cf9d8b1b684934d3b501bc31
matthiasva <matthiasva@edu.aau.at>
2025-04-21 17:07:30 +0200

    #15 - Rolling the dice: - added ROLL_DICE and DICE_RESULT to MessageType enum

--- src/main/java/com/example/cataniaunited/dto/MessageType.java
+++ src/main/java/com/example/cataniaunited/dto/MessageType.java
@@ -9,11 +9,13 @@ public enum MessageType {
     LOBBY_UPDATED,
     PLAYER_JOINED,
     LOBBY_CREATED,
+    DICE_RESULT,
 
     //Client Messages
     CREATE_LOBBY,
     JOIN_LOBBY,
     SET_USERNAME,
     PLACE_SETTLEMENT,
-    PLACE_ROAD
+    PLACE_ROAD,
+    ROLL_DICE
 }
