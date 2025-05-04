730c750057131a426db62d11bfc803e0a56c0551
matthiasva <matthiasva@edu.aau.at>
2025-05-04 17:21:49 +0200

    #15 - Rolling the dice: - implemented Subscriber interface in Tile to receive dice updates and flag resource availability

--- src/main/java/com/example/cataniaunited/game/board/tile_list_builder/Tile.java
+++ src/main/java/com/example/cataniaunited/game/board/tile_list_builder/Tile.java
@@ -1,17 +1,21 @@
 package com.example.cataniaunited.game.board.tile_list_builder;
 
 import com.example.cataniaunited.game.board.Placable;
+import com.example.cataniaunited.Subscriber;
+import com.example.cataniaunited.game.dice.Dice;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-public class Tile implements Placable {
+public class Tile implements Placable, Subscriber<Dice, Integer> {
     final TileType type;
     int value = 0; // To set later
 
     double[] coordinates = new double[2];
 
     int id;
+
+    private boolean hasResource = false;
 
     public Tile(TileType type) {
         this.type = type;
@@ -80,5 +84,25 @@ public class Tile implements Placable {
         tileNode.set("coordinates", coordsNode);
 
         return tileNode;
+    }
+
+    public void subscribeToDice(Dice dice) {
+        dice.addSubscriber(this);
+    }
+
+    @Override
+    public void update(Integer diceValue) {
+        if (this.value == diceValue && this.type != TileType.WASTE) {
+            this.hasResource = true;
+            // notify buildings here
+        }
+    }
+
+    public boolean hasResource() {
+        return hasResource;
+    }
+
+    public void resetResource() {
+        this.hasResource = false;
     }
 }
