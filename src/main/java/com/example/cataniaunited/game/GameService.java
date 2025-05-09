7b8792b9a6d25a16e6e59b7eaf08b63873cf0da5
matthiasva <matthiasva@edu.aau.at>
2025-05-09 19:10:24 +0200

    15-Rolling_the_dice - made return for rollDice cleaner

--- src/main/java/com/example/cataniaunited/game/GameService.java
+++ src/main/java/com/example/cataniaunited/game/GameService.java
@@ -67,7 +67,6 @@ public class GameService {
     }
 
     public ObjectNode rollDice(String lobbyId) throws GameException {
-        GameBoard gameboard = getGameboardByLobbyId(lobbyId);
-        return gameboard.rollDice();
+        return getGameboardByLobbyId(lobbyId).rollDice();
     }
 }
